2a62dd7173987d5f0b512e482d09c331b3b8499c
Nelson Earle <nelson.earle137@gmail.com>
2017-01-25 04:20:27 -0600

    moved python and cpp scripts into projects of their lang

--- CPP Scripts/bar.cpp
+++ CPP Scripts/bar.cpp
@@ -10,15 +10,11 @@
 using namespace std;
 
 bool file_exists(string filename) {
-    
     ifstream file(filename);
-    
     return file.good();
-    
 }
 
 string get_cwd() {
-    
     string data;
     FILE * stream;
     int buffer_size = 100;
@@ -26,35 +22,25 @@ string get_cwd() {
     
     stream = popen("pwd", "r");
     if (stream) {
-        
         while (!feof(stream)) {
-            
             if (fgets(buffer, buffer_size, stream) != NULL) data.append(buffer);
-            
         }
-        
     }
-    
+
     return data;
-    
 }
 
 int main(int argc, char* argv[]) {
-    
     if (argc == 2) {
-        
         string command = "g++ ";
         command += argv[1];
         
         system(command.c_str());
         system("./a.out");
         system("rm ./a.out");
-        
     } else {
-        
         cout << "Usage: bar [file]" << endl;
         cout << "NOTE: Call in directory of file" << endl;
-        
     }
     
     return 0;
