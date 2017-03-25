ce48e4b6ac8db790e9e8edf82c72b219b8772989
Nelson Earle <nelson137@users.noreply.github.com>
2017-03-24 19:29:03 -0500

    created bashrc-functions.sh

---
+++ Bash/bashrc-functions.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+mkcd() {
+  if [ "$#" -ne "1" ]; then
+    echo "E: mkcd can only take 1 argument: [dir]"
+  else
+    mkdir -p -- "$1" && cd -P -- "$1"
+  fi
+}
+
+border-text() {
+  b="#"
+  lines=("$@")
+  widest="0"
+  for l in "${lines[@]}"; do if [ "${#l}" -gt "$widest" ]; then widest="${#l}"; fi; done
+  full_widest="$((widest+4))"
+  echo $(printf -- "$b%.0s" $(seq 1 $full_widest))
+  for l in "${lines[@]}"; do
+    width="${#l}"
+    if [ "$width" != "$widest" ]; then
+      echo "$b $l`printf ' %.0s' $(seq 1 $((widest-width)))` $b"
+    else
+      echo "$b $l $b"
+    fi
+  done
+  echo $(printf -- "$b%.0s" $(seq 1 $full_widest))
+}
+
+mygpp() {
+  if [ "$@" -ne "1" ]; then
+    echo "E: mygpp can only take 1 argument: [input_file]"
+  else
+    g++ "$1" -o "${1%.cpp}"
+  fi
+}
