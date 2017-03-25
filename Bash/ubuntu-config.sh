37410fccf2c959d81012d47ae0f5bb9c06cdf30a
Nelson Earle <nelson137@users.noreply.github.com>
2017-03-25 10:11:08 -0500

    updated error messages

--- Bash/ubuntu-config.sh
+++ Bash/ubuntu-config.sh
@@ -20,38 +20,38 @@ errors=("Errors:")
 
 installations() {
 	# apt
-	sudo apt-add-repository ppa:webupd8team/sublime-text-3 -y || errors+=("Adding the Sublime Text 3 source to sources.list")
-	sudo apt-get update || errors+=("Updating the system")
-	sudo apt-get upgrade -y || errors+=("Upgrading the system")
+	sudo apt-add-repository ppa:webupd8team/sublime-text-3 -y || errors+=("adding the Sublime Text 3 source to sources.list")
+	sudo apt-get update || errors+=("updating system")
+	sudo apt-get upgrade -y || errors+=("upgrading system")
 	sudo apt-get install sl vim git virtualenv libxss1 libappindicator1 libindicator7 sublime-text-installer apache2 -y || errors+=("apt-get installations")
-	sudo chown -R `whoami`:`whoami` /var/www || errors+=("Giving user full permissions to /var/www")
+	sudo chown -R `whoami`:`whoami` /var/www || errors+=("giving user full permissions to /var/www")
 
 	# git
-	git clone https://github.com/nelson137/scripts.git "$HOME/Projects/Git/scripts" || errors+=("Cloning the scripts repository from Github")
+	git clone https://github.com/nelson137/scripts.git "$HOME/Projects/Git/scripts" || errors+=("cloning scripts repository from Github")
 
 	# Google Grive
-	wget -O "$HOME/Pictures/orion-nebula.jpg" "https://drive.google.com/uc?id=0B3AM8GpU5FlwVlF3REMyQ1FnTTg&export=download" || errors+=("Downloading the orion-nebula.jpg wallpaper from Google Drive")
-	wget -O "$HOME/Downloads/vault-key.zip" "https://drive.google.com/uc?id=0B3AM8GpU5FlwUHZiM0JIamxJc0E&export=download" &&
-	unzip "$HOME/Downloads/vault-key.zip" -d "$HOME/Projects/Web/Flask/" &&
-	rm "$HOME/Downloads/vault-key.zip" || errors+=("Downloading vault-key.zip from Google Drive")
+	wget -O "$HOME/Pictures/orion-nebula.jpg" "https://drive.google.com/uc?id=0B3AM8GpU5FlwVlF3REMyQ1FnTTg&export=download" || errors+=("downloading orion-nebula.jpg wallpaper from Google Drive")
+	wget -O "$HOME/Downloads/vault-key.zip" "https://drive.google.com/uc?id=0B3AM8GpU5FlwUHZiM0JIamxJc0E&export=download" || errors+=("vault-key setup: download")
+	unzip "$HOME/Downloads/vault-key.zip" -d "$HOME/Projects/Web/Flask/" || errors+=("vault-key setup: unzip")
+	rm "$HOME/Downloads/vault-key.zip" || errors+=("vault-key setup: delete zip")
 	
 	# Google Chrome
 	wget -O "$HOME/Downloads/google-chrome.deb" "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" &&
 	sudo apt-get install -f -y &&
 	sudo dpkg -i "$HOME/Downloads/google-chrome.deb" &&
-	rm "$HOME/Downloads/google-chrome.deb" || errors+=("Downloading Google Chrome")
+	rm "$HOME/Downloads/google-chrome.deb" || errors+=("Google Chrome: downloading")
 
 	# Tor
 	wget -O "$HOME/Downloads/tor.tar.xz" "https://github.com/TheTorProject/gettorbrowser/releases/download/v6.5.1/tor-browser-linux64-6.5.1_en-US.tar.xz" &&
 	mkdir "$HOME/tor" && tar -xf "$HOME/Downloads/tor.tar.xz" -C "$HOME/tor/" --strip-components=1 &&
-	rm "$HOME/Downloads/tor.tar.xz" || errors+=("Downloading Tor")
+	rm "$HOME/Downloads/tor.tar.xz" || errors+=("Tor: downloading")
 }
 
 
 
 system() {
 	# System Settings
-	gsettings set org.gnome.desktop.session idle-delay 1800 || errors+=("Changing the delay for turning the screen off when inactive")
+	gsettings set org.gnome.desktop.session idle-delay 1800 || errors+=("system settings: changing turn the screen off when inactive delay")
 
 	# ~/.bashrc
 	bachrc_text='source ~/.bash_additions'
@@ -139,14 +139,14 @@ Hidden=false'
 	for s in ${scripts[@]}; do
 		sans_ext="${s%.sh}"
 		if [ ! -f "$HOME/bin/$sans_ext" ]; then
-			ln -s "$HOME/Projects/Git/scripts/Bash/$s" "$HOME/bin/$sans_ext" || errors+=("Creating ~/bin links")
+			ln -s "$HOME/Projects/Git/scripts/Bash/$s" "$HOME/bin/$sans_ext" || errors+=("creating ~/bin links")
 		fi
 	done
 
 	# Virtualenvs
-	virtualenv -p python3.5 "$HOME/.virtualenvs/MainEnv" || errors+=("Creating MainEnv")
-	source "$HOME/.virtualenvs/MainEnv/bin/activate" || errors+=("Sourcing MainEnv")
-	pip install myplatform flask requests || errors+=("Installing myplatform, flask, and requests to MainEnv")
+	virtualenv -p python3.5 "$HOME/.virtualenvs/MainEnv" || errors+=("MainEnv: create")
+	source "$HOME/.virtualenvs/MainEnv/bin/activate" || errors+=("MainEnv: source")
+	pip install myplatform flask requests || errors+=("MainEnv: installing myplatform, flask, and requests")
 }
 
 
@@ -156,16 +156,16 @@ visuals() {
 	gsettings set com.canonical.Unity.Launcher favorites '["unity://expo-icon","application://firefox.desktop","application://google-chrome.desktop","application://gnome-terminal.desktop","application://org.gnome.Nautilus.desktop","application://sublime-text.desktop","unity://running-apps"]' || errors+=("Setting the Launcher Favorites order")
 
 	# Wallpaper
-	gsettings set org.gnome.desktop.background picture-uri "file://$HOME/Pictures/orion-nebula.jpg" || errors+=("Setting the wallpaper")
+	gsettings set org.gnome.desktop.background picture-uri "file://$HOME/Pictures/orion-nebula.jpg" || errors+=("setting wallpaper")
 
 	# Terminal Profile
 	profile="/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9"
-	dconf write "$profile/visible-name" "'Main'" || errors+=("Changing the terminal profile's settings")
-	#dconf write "$profile/default-size-columns" 80 || errors+=("Changing the terminal profile's settings" #default=80)
-	#dconf write "$profile/default-size-rows 24" || errors+=("Changing the terminal profile's settings" #default=24)
-	dconf write "$profile/use-transparent-background" true || errors+=("Changing the terminal profile's settings")
-	dconf write "$profile/background-transparency-percent" 20 || errors+=("Changing the terminal profile's settings")
-	dconf write "$profile/cursor-shape" "'ibeam'" || errors+=("Changing the terminal profile's settings")
+	dconf write "$profile/visible-name" "'Main'" || errors+=("terminal profile settings: name")
+	#dconf write "$profile/default-size-columns" 80 || errors+=("terminal profile settings: columns") #default=80
+	#dconf write "$profile/default-size-rows 24" || errors+=("terminal profile settings: rows") #default=24
+	dconf write "$profile/use-transparent-background" true || errors+=("terminal profile settings: transparent bg")
+	dconf write "$profile/background-transparency-percent" 20 || errors+=("terminal profile settings: transparent bg %")
+	dconf write "$profile/cursor-shape" "'ibeam'" || errors+=("terminal profile settings: cursor shape")
 }
 
 
@@ -209,18 +209,18 @@ programs() {
 
 	if [ -f "$HOME/.mozilla/firefox/$profile/user.js" ]; then
 	    echo "
-$firefox_user_text" >> "$HOME/.mozilla/firefox/$profile/user.js" || errors+=("Changing Firefox settings")
+$firefox_user_text" >> "$HOME/.mozilla/firefox/$profile/user.js" || errors+=("Firefox: settings")
 	else
-	    echo "$firefox_user_text" > "$HOME/.mozilla/firefox/$profile/user.js" || errors+=("Changing Firefox settings")
+	    echo "$firefox_user_text" > "$HOME/.mozilla/firefox/$profile/user.js" || errors+=("Firefox: settings")
 	fi
 
 	# Google Chrome
-	google-chrome && sleep 3 && kill -9 "$(pgrep google-chrome)" || errors+=("Starting then stopping Google Chrome")
+	google-chrome && sleep 3 && kill -9 "$(pgrep google-chrome)" || errors+=("Google Chrome: starting then stopping")
 	
 	# Sublime Text
-	subl && sleep 2 && kill -9 "$(pgrep subl)" || errors+=("Starting then stopping Sublime Text")
+	subl && sleep 2 && kill -9 "$(pgrep subl)" || errors+=("Sublime Text: starting then stopping")
 
-	wget -O "$HOME/.config/sublime-text-3/Installed Packages/Package Control.sublime-package" "https://packagecontrol.io/Package%20Control.sublime-package" || errors+=("Downloading the Sublime Text Package Control")
+	wget -O "$HOME/.config/sublime-text-3/Installed Packages/Package Control.sublime-package" "https://packagecontrol.io/Package%20Control.sublime-package" || errors+=("Sublime Text: downloading Package Control")
 
 	installed_packages_text='{
 		"installed_packages":
@@ -233,7 +233,7 @@ $firefox_user_text" >> "$HOME/.mozilla/firefox/$profile/user.js" || errors+=("Ch
 			"Virtualenv"
 		]
 	}'
-	echo "$installed_packages_text" > "$HOME/.config/sublime-text-3/Packages/User/Package Control.sublime-settings" || errors+=("Writing the Sublime Text Package Control file for installed_packages")
+	echo "$installed_packages_text" > "$HOME/.config/sublime-text-3/Packages/User/Package Control.sublime-settings" || errors+=("Sublime Text: Package Control installed_packages file")
 }
 
 
