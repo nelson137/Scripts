2d5e0f2e168e0a832920b17e6b44842b927d3b75
Nelson Earle <nelson137@users.noreply.github.com>
2017-04-15 02:16:00 -0500

    replaced [ ] with [[ ]]

--- Bash/qtmux.sh
+++ Bash/qtmux.sh
@@ -7,22 +7,22 @@ name="Temp"
 nsplits="0"
 
 for i in "${!args[@]}"; do
-	if [ "${args[$i]}" == "-n" ]; then
+	if [[ ${args[$i]} == -n ]]; then
 		name="${args[$((i+1))]}"
-	elif [ "${args[$i]}" == "-s" ]; then
+	elif [[ ${args[$i]} == -s ]]; then
 		npanes="${args[$((i+1))]}"
 	fi
 done
 
 tmux new -s "$name" -n "$name" -d
 
-if [ "$npanes" -lt "5" ]; then
-	if [ "$npanes" == "2" ]; then
+if [[ $npanes -lt 5 ]]; then
+	if [[ $npanes == 2 ]]; then
 		tmux splitw -t 0
-	elif [ "$npanes" == "3" ]; then
+	elif [[ $npanes == 3 ]]; then
 		tmux splitw -t 0 -p 33
 		tmux splitw -t 0
-	elif [ "$npanes" == "4" ]; then
+	elif [[ $npanes == 4 ]]; then
 		tmux splitw -t 0
 		tmux splitw -t 0
 		tmux splitw -t 1
