30107b0ea106bd2579ae417d467b7715e674d9ba
nelson137 <Nelson@Nelson-MacBook.local>
2017-07-23 19:24:35 -0500

    added entry_validation() to Tkinter

--- Python/Tkinter.py
+++ Python/Tkinter.py
@@ -1,12 +1,16 @@
-from tkinter import Tk, Frame, Label, Listbox, END, MULTIPLE
+from tkinter import END, Entry, Frame, Label, Listbox, Tk
+from tkinter.constants import *
 
 class App(Tk):
+    """Demonstrate concepts in tkinter."""
+
 	def __init__(self):
 		super().__init__()
 		self.protocol('WM_DELETE_WINDOW', self.onExit) # override onExit
 
 		self.synced_listboxes()
 		self.multiselect_synced_listboxes()
+		self.entry_validation()
 
 	def synced_listboxes(self):
 		"""Create two Listboxes whose selected items are synced."""
@@ -20,7 +24,8 @@ class App(Tk):
 		wrapper = Frame(self)
 		wrapper.pack()
 
-		Label(wrapper, text='Synced Listboxes').grid(row=0, column=0, columnspan=2)
+		label = Label(wrapper, text='Synced Listboxes')
+        label.grid(row=0, column=0, columnspan=2)
 
 		lb1 = Listbox(wrapper, exportselection=False)
 		lb1.bind('<<ListboxSelect>>', lambda e: syncedSelect(e))
@@ -35,7 +40,9 @@ class App(Tk):
 		lbs = [lb1, lb2]
 
 	def multiselect_synced_listboxes(self):
-		"""Create two Listboxes in selectmode MULTIPLE whose selected items are synced."""
+		"""Create two Listboxes in selectmode
+        MULTIPLE whose selected items are synced.
+        """
 
 		def multiSyncedSelect(e):
 			for lb in lbs:
@@ -47,7 +54,8 @@ class App(Tk):
 		wrapper = Frame(self)
 		wrapper.pack()
 
-		Label(wrapper, text='Multi-Select Synced Listboxes').grid(row=0, column=0, columnspan=2)
+		label = Label(wrapper, text='Multi-Select Synced Listboxes')
+        label.grid(row=0, column=0, columnspan=2)
 
 		lb1 = Listbox(wrapper, selectmode=MULTIPLE, exportselection=False)
 		lb1.bind('<<ListboxSelect>>', lambda e: multiSyncedSelect(e))
@@ -61,8 +69,51 @@ class App(Tk):
 
 		lbs = [lb1, lb2]
 
+	def entry_validation(self):
+        """Create an entry that will only accept numeric input."""
+
+		def onValidate(S):
+			if S in '0123456789':
+				return True
+			return False
+		def onBad(S):
+			print('Invalid input:', S)
+
+		wrapper = Frame(self)
+		wrapper.pack()
+
+		vcmd = (self.register(onValidate), '%S')
+		invcmd = (self.register(onBad), '%S')
+		Entry(wrapper, validate='key', vcmd=vcmd, invcmd=invcmd).pack()
+
+		"""
+		Callback substitution codes:
+		+------+--------------------------------------------------------------+
+		| '%d' | 0 for an attempted deletion, 1 for an attempted insertion,   |
+		|      | or -1 if the callback was called for focus in, focus out,    |
+		|      | or a change to the textvariable.                             |
+		+------+--------------------------------------------------------------+
+		| '%i' | The index of the beginning of the attempted insertion or     |
+		|      | deletion. -1 if the callback was due to focus in, focus out, |
+		|      | or a change to the textvariable.                             |
+		+------+--------------------------------------------------------------+
+		| '%P' | The value that the text will have if the change is allowed.  |
+		+------+--------------------------------------------------------------+
+		| '%s' | The text in the entry before the change.                     |
+		+------+--------------------------------------------------------------+
+		| '%S' | The text of the insertion or deletion.                       |
+		+------+--------------------------------------------------------------+
+		| '%v' | The current value of the entry's validate option.            |
+		+------+--------------------------------------------------------------+
+		| '%V' | The reason for this callback: 'focusin', 'focusout', 'key',  |
+		|      | or 'forced' if the textvariable was changed.                 |
+		+------+--------------------------------------------------------------+
+		| '%W' | The name of the widget.                                      |
+		+------+--------------------------------------------------------------+
+		"""
+
 	def onExit(self):
-		"""Overrides tkinter's onExit."""
+		"""Perform tasks when the application is exited."""
 
 		print('Quitting application...')
 		self.destroy()
--- Python/Twilio.py
+++ Python/Twilio.py
@@ -1,18 +1,16 @@
-from twilio.rest import TwilioRestClient
+from twilio.rest import Client
 
 emergency_account_access_code = "RfP5XPc9FUBtBmQ7eMQ9AIptUgeJg3lOdnYKttZn"
 
 ACCOUNT_SID = "ACb68adc9e3caabc74f3a63ca330955e6a"
 AUTH_TOKEN = "475362ce624b1f248f7c2b24421ad003"
 
-client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
-
-body_message = raw_input("Message Body: ")
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
 
 message = client.messages.create(
-    body="what time will you be home?",
+    body="",
     to="+16366149325",
-    from_="+16367289229"
+    from_="+13143845859"
 )
 
 print(message.sid)
