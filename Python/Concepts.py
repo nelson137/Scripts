b1ed94d4eea61cc317e64a3f62a8003c453da044
nelson137 <Nelson@Nelson-MacBook.local>
2017-07-27 17:53:23 -0500

    converted from class to funcs, added module doc

--- Python/Concepts.py
+++ Python/Concepts.py
@@ -1,51 +1,51 @@
-from subprocess import Popen, PIPE
-from collections import OrderedDict
+#!/usr/bin/env python3
 
-class Concepts:
-    """Demonstrate concepts from the standard library."""
+"""Usage: ./Concepts.py"""
 
-    def __init__(self):
-        self.subprocess()
-        self.orderedDict()
+def subprocess():
+    """Run a bash command and print its output."""
+    from subprocess import Popen, PIPE
 
-    def subprocess(self):
-        """Runs bash command and get output"""
-        process = Popen('ls /home/nelson/Projects/Git', stdout=PIPE, shell=True)
-        out, err = process.communicate()
-        print(out.decode())
+    process = Popen('ls /home/nelson/Projects/Git', stdout=PIPE, shell=True)
+    out, err = process.communicate()
+    print(out.decode())
 
-    def orderedDict(self):
-        """Sorts an ordered dict by key, with a custom key list, and with secondary sorting"""
-        data = OrderedDict([('model 6', OrderedDict([('model', 'model 6'),
-                                                     ('production status', 'flight-ready')])),
-                            ('model 7', OrderedDict([('model', 'model 7'),
-                                                     ('production status', 'flight-ready')])),
-                            ('model 1', OrderedDict([('model', 'model 1'),
-                                                     ('production status', 'in-production')])),
-                            ('model 5', OrderedDict([('model', 'model 5'),
-                                                     ('production status', 'flight-ready')])),
-                            ('model 2', OrderedDict([('model', 'model 2'),
-                                                     ('production status', 'announced')])),
-                            ('model 4', OrderedDict([('model', 'model 4'),
-                                                     ('production status', 'in-concept')])),
-                            ('model 3', OrderedDict([('model', 'model 3'),
-                                                     ('production status', 'flight-ready')]))])
+def sort_od():
+    """Sort an OrderedDict using three different methods."""
+    from collections import OrderedDict
 
-        byModel = OrderedDict(sorted(data.items(), key=lambda item: item[0]))
-        byProdStat = OrderedDict(sorted(data.items(), key=lambda item: ['flight-ready', 'hangar-ready', 'ready', 'in-production', 'in-concept', 'announced'].index(item[1]['production status'])))
-        byProdStatWithSecondary = OrderedDict(sorted(data.items(), key=lambda item: (['flight-ready', 'hangar-ready', 'ready', 'in-production', 'in-concept', 'announced'].index(item[1]['production status']), item[0])))
+    data = OrderedDict([('model 6', OrderedDict([('model', 'model 6'), ('production status', 'flight-ready')])),
+                        ('model 7', OrderedDict([('model', 'model 7'), ('production status', 'flight-ready')])),
+                        ('model 1', OrderedDict([('model', 'model 1'), ('production status', 'in-production')])),
+                        ('model 5', OrderedDict([('model', 'model 5'), ('production status', 'flight-ready')])),
+                        ('model 2', OrderedDict([('model', 'model 2'), ('production status', 'announced')])),
+                        ('model 4', OrderedDict([('model', 'model 4'), ('production status', 'in-concept')])),
+                        ('model 3', OrderedDict([('model', 'model 3'), ('production status', 'flight-ready')]))])
 
-        print('Primary sorted by Model:')
-        for m in byModel.items():
-            print(m)
+    byModel = OrderedDict(sorted(data.items(), key=lambda item: item[0]))
+    byProdStat = OrderedDict(sorted(data.items(), key=lambda item: ['flight-ready', 'hangar-ready', 'ready', 'in-production', 'in-concept', 'announced'].index(item[1]['production status'])))
+    byProdStatWithSecondary = OrderedDict(sorted(data.items(), key=lambda item: (['flight-ready', 'hangar-ready', 'ready', 'in-production', 'in-concept', 'announced'].index(item[1]['production status']), item[0])))
 
-        print('\nPrimary sorted by Production Status:')
-        for p in byProdStat.items():
-            print(p)
-        
-        print('\nPrimary sorted by Production Status, secondary sorted by Model:')
-        for ps in byProdStatWithSecondary.items():
-            print(ps)
+    print('Primary sorted by Model:')
+    for m in byModel.items():
+        print(m)
+
+    print('\nPrimary sorted by Production Status:')
+    for p in byProdStat.items():
+        print(p)
+    
+    print('\nPrimary sorted by Production Status, secondary sorted by Model:')
+    for ps in byProdStatWithSecondary.items():
+        print(ps)
+
+def main():
+    subprocess()
+    sort_od()
 
 if __name__ == '__main__':
-    Concepts()
+    from sys import argv
+
+    if len(argv) == 1:
+        main()
+    else:
+        print(__doc__) # print the module doc
